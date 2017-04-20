81d11c616df976ef7d1bf419d37eb3e131a1a2cf
Slade Baumann <sladebaumann@yahoo.com>
2017-04-20 10:30:26 -0500

    Ability to write to DB added
    This commit finished up the work that was started in
    the original "PUT" commit. Writes to DB when
    existing ID is used in PUT request.
    
    Also moved common items to common repo (such as getting
    external_api info, etc).

--- myretail_restful_service/products/products.py
+++ myretail_restful_service/products/products.py
@@ -4,6 +4,7 @@ import redis
 import urllib2
 
 from collections import OrderedDict
+from myretail_restful_service.common import common
 
 REDIS_DB = redis.StrictRedis(host='localhost', port=6379, db=0)
 
@@ -15,45 +16,41 @@ class Resource(object):
         resp.status = falcon.HTTP_200
     def on_put(self, req, resp, id):
         new_product = req.stream.read()
-        write_data = WriteData()
-        # TODO: fill in WriteData class to write to DB new price
-        write_data.write_price_to_db(id, new_product)
+        write_data = WriteData().write_price_to_db(id, new_product)
+        resp.body = write_data
         resp.status = falcon.HTTP_200
 
 
 class WriteData(object):
-    # TODO: below
-    # verify id is in current external api and database
-    # verify id in new_product is same as id
-    # pull out price info from new_product
-    # write price to redis hash
-    # return True or something happy?
-    # was using `http PUT localhost:8000/products/13860428 product:=@./product_info.json``
-    # to call, but postman (chrome plugin) may be best option. figure out how to
-    # put json with postman
     def write_price_to_db(self, id, new_product):
-        print('nothing')
-        # verify that id is in
+        new_value = self.get_new_product_price(new_product)
+        external_api = common.Common().get_external_api()
+        product_id = common.Common().verify_id_exists(id, external_api)
+        if REDIS_DB.hexists(product_id, "value"):
+            REDIS_DB.hset(product_id, "value", new_value)
+            return ('New Product Value Written to Database Successfully')
+        else:
+            return (
+                'Products ID does not exist, so value has not been written. '
+                'Please try again with valid JSON.')
+
     def get_new_product_price(self, new_product):
-        print('nothing')
+        # verify that id is in
+        new_product_dict = json.loads(new_product)
+        new_value = new_product_dict["current_price"]["value"]
+        return new_value
 
 
 class RetrieveData(object):
     def get_external_data(self, id):
         external_dict = {}
-        info = json.load(urllib2.urlopen(
-            "http://redsky.target.com/v1/pdp/tcin/13860428?excludes=taxonomy,price,promotion,bulk_ship,rating_and_review_reviews,rating_and_review_statistics,question_answer_statistics"))
-
+        # get external api from common method
+        external_api = common.Common().get_external_api()
         # check if id that is passed it is in the external API
-        if (info['product']['available_to_promise_network'][
-            'product_id']) == id:
-            self.product_id = int(id)
-            # TODO: put falcon error handling in here in an else statement)
-            # aka return failure or something here. If it doesn't find
-            # ID, then falcon should return "missing ID"
-        title = (info['product']['item']['product_description']['title'])
+        product_id = common.Common().verify_id_exists(id, external_api)
+        title = (external_api['product']['item']['product_description']['title'])
 
-        external_dict['id'] = self.product_id
+        external_dict['id'] = product_id
         external_dict['name'] = title
 
         return external_dict
@@ -61,13 +58,6 @@ class RetrieveData(object):
     def get_database_data(self, id):
         price_dict = {}
         # use id from URL
-        # hardcoded value inside redis db for now.
-        # TODO: Will write code when post/database manipulation is setup
-        # Using HMSET to set value based on 'id' being the key
-        # and the values must go in reverse order as you want printed
-        # `redis-cli HMSET 13860428 currency_code USD value 13.49`
-        # `redis-cli HGETALL 13860428`
-        # redis command to get entire hash (dict in python) based on id
         current_price = REDIS_DB.hgetall(id)
         # convert price from string that redis stores to float
         current_price['value'] = float(current_price['value'])
